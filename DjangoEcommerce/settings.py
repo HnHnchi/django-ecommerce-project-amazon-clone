00e9f83681548f6b3586e8a4ad63adc89ba5e5eb
HnHnchi <168960528+HnHnchi@users.noreply.github.com>
2025-07-29 01:00:31 +0000

    authentication

--- DjangoEcommerce/settings.py
+++ DjangoEcommerce/settings.py
@@ -129,3 +129,9 @@ MEDIA_ROOT=os.path.join(BASE_DIR,"media")
 STATIC_URL="/static/"
 STATIC_ROOT=os.path.join(BASE_DIR,"static")
 BASE_URL="http://127.0.0.1:8000"
+CSRF_TRUSTED_ORIGINS = [
+    "http://127.0.0.1:8000",
+    "http://localhost:8000",
+    "https://127.0.0.1:8000",
+    "https://localhost:8000"
+]
--- DjangoEcommerceApp/views.py
+++ DjangoEcommerceApp/views.py
@@ -3,8 +3,40 @@ from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from django.urls import reverse
+from DjangoEcommerceApp.models import CustomUser as User
+from django.shortcuts import redirect
+
+
+
 
 # Create your views here.
+def createAccount(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password1 = request.POST.get("password1")
+        password2 = request.POST.get("password2")
+
+        if password1 != password2:
+            messages.error(request, "Passwords do not match!")
+            return redirect("create_account")
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already taken!")
+            return redirect("create_account")
+
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "Email already used!")
+            return redirect("create_account")
+
+        user = User.objects.create_user(username=username, email=email, password=password1)
+        user.save()
+        messages.success(request, "Account created successfully! Please log in.")
+        return redirect("admin_login")
+
+    return render(request, "create.html")
+def home(request):
+    return render(request, "demo.html")
 def demoPage(request):
     return HttpResponse("demo Page")
 
